bd8f56104cdec2fe548cb7665e187f5fe23401ad
Matej Skalicky <matej.skalickyy@gmail.com>
2025-03-09 21:18:05 +0100

    cpp fmt

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -254,7 +254,7 @@ return { -- LSP Plugins
 				-- Disable "format_on_save lsp_fallback" for languages that don't
 				-- have a well standardized coding style. You can add additional
 				-- languages here or re-enable it for the disabled ones.
-				local disable_filetypes = { c = true, cpp = true }
+				local disable_filetypes = { c = true }
 				local lsp_format_opt
 				if disable_filetypes[vim.bo[bufnr].filetype] then
 					lsp_format_opt = "never"
@@ -268,6 +268,7 @@ return { -- LSP Plugins
 			end,
 			formatters_by_ft = {
 				lua = { "stylua" },
+				cpp = { "clang-format" },
 				-- Conform can also run multiple formatters sequentially
 				-- python = { "isort", "black" },
 				--
