af6468f8b20616670b3790920972b15bcb7d225f
Matej Skalicky <matej.skalickyy@gmail.com>
2025-04-03 13:10:17 +0200

    vague ftw

--- lua/plugins/barvy.lua
+++ lua/plugins/barvy.lua
@@ -12,21 +12,21 @@ return {
 	-- 		vim.cmd.colorscheme("rose-pine")
 	-- 	end,
 	-- },
-	{
-		"rebelot/kanagawa.nvim",
-		priority = 1000,
-		init = function()
-			-- vim.cmd.colorscheme("kanagawa-wave")
-			vim.cmd.colorscheme("kanagawa-dragon")
-			-- vim.cmd.colorscheme("kanagawa-lotus")
-		end,
-	},
 	-- {
-	-- 	"vague2k/vague.nvim",
-	-- 	config = function()
-	-- 		require("vague").setup({
-	-- 			vim.cmd.colorscheme("vague"),
-	-- 		})
+	-- 	"rebelot/kanagawa.nvim",
+	-- 	priority = 1000,
+	-- 	init = function()
+	-- 		-- vim.cmd.colorscheme("kanagawa-wave")
+	-- 		vim.cmd.colorscheme("kanagawa-dragon")
+	-- 		-- vim.cmd.colorscheme("kanagawa-lotus")
 	-- 	end,
 	-- },
+	{
+		"vague2k/vague.nvim",
+		config = function()
+			require("vague").setup({
+				vim.cmd.colorscheme("vague"),
+			})
+		end,
+	},
 }
