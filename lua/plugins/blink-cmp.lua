c73b76d9faf3175981904e0cf93cd9c7b563db73
Matej Skalicky <matej.skalickyy@gmail.com>
2025-05-26 17:04:27 +0200

    bling

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -1,6 +1,9 @@
 return {
 	"saghen/blink.cmp",
-	dependencies = { "rafamadriz/friendly-snippets" },
+	dependencies = {
+		"rafamadriz/friendly-snippets",
+		"moyiz/blink-emoji.nvim",
+	},
 
 	version = "1.*",
 
@@ -11,10 +14,43 @@ return {
 			nerd_font_variant = "mono",
 		},
 
-		completion = { documentation = { auto_show = true } },
+		completion = {
+			documentation = {
+				auto_show = true,
+				window = {
+					border = "single",
+				},
+			},
+			menu = {
+				border = "single",
+			},
+		},
+
+		snippets = { preset = "luasnip" },
 
 		sources = {
-			default = { "lsp", "path", "snippets", "buffer" },
+			default = { "lsp", "path", "snippets", "buffer", "emoji" },
+
+			providers = {
+				emoji = {
+					module = "blink-emoji",
+					name = "Emoji",
+					score_offset = 15, -- Tune by preference
+					opts = {
+						insert = true, -- Insert emoji (default) or complete its name
+						---@type string|table|fun():table
+						trigger = function()
+							return { ":" }
+						end,
+					},
+					should_show_items = function()
+						return vim.tbl_contains(
+							{ "gitcommit", "markdown", "html", "typescript", "typescriptreact" },
+							vim.o.filetype
+						)
+					end,
+				},
+			},
 		},
 
 		fuzzy = { implementation = "prefer_rust_with_warning" },
