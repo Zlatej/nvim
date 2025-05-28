bb6615b8f30606f967e21407e5c6a23d02c5ef0d
Matej Skalicky <matej.skalickyy@gmail.com>
2025-05-28 18:49:25 +0200

    fix?

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -1,10 +1,6 @@
 return {
 	"saghen/blink.cmp",
-	dependencies = {
-		"rafamadriz/friendly-snippets",
-		"moyiz/blink-emoji.nvim",
-        "L3MON4D3/LuaSnip",
-	},
+	dependencies = { "rafamadriz/friendly-snippets" },
 
 	version = "1.*",
 
@@ -12,6 +8,7 @@ return {
 		keymap = { preset = "default" },
 
 		appearance = {
+			use_nvim_cmp_as_default = false,
 			nerd_font_variant = "mono",
 		},
 
@@ -22,40 +19,18 @@ return {
 					border = "single",
 				},
 			},
-			menu = {
-				border = "single",
-			},
 		},
 
-		snippets = { preset = "luasnip" },
+		signature = {
+			enabled = true,
+			window = { border = "single" },
+		},
 
 		sources = {
-			default = { "lsp", "path", "snippets", "buffer", "emoji" },
-
-			providers = {
-				emoji = {
-					module = "blink-emoji",
-					name = "Emoji",
-					score_offset = 15, -- Tune by preference
-					opts = {
-						insert = true, -- Insert emoji (default) or complete its name
-						---@type string|table|fun():table
-						trigger = function()
-							return { ":" }
-						end,
-					},
-					should_show_items = function()
-						return vim.tbl_contains(
-							{ "gitcommit", "markdown", "html", "typescript", "typescriptreact" },
-							vim.o.filetype
-						)
-					end,
-				},
-			},
+			default = { "lsp", "path", "snippets", "buffer" },
 		},
 
 		fuzzy = { implementation = "prefer_rust_with_warning" },
-		signature = { enabled = true },
 	},
 	opts_extend = { "sources.default" },
 }
