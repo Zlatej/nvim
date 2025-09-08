4ef503854debcb1719d816285e074d8d328d7172
Matej Skalicky <matej.skalickyy@gmail.com>
2025-09-08 18:22:56 +0200

    cpy

--- lua/plugins/copy.lua
+++ lua/plugins/copy.lua
@@ -2,12 +2,7 @@ return {
 	"zlatej/copy.nvim",
 	config = function()
 		require("copy").setup({
-            keymap = {
-                line = "<leader>cc",
-            },
-            -- Prefix to strip absolute path
-            prefix = "/home/matej/repos/",
-        })
+			prefix = "/home/matej/repos/",
+		})
 	end,
 }
-
