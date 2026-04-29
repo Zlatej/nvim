b3d4e356c50508b788144a4002c79f87dbc51a22
Matej Skalicky <matej.skalicky@proton.me>
2026-04-29 08:48:09 +0200

    Update refactoring.nivm

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -47,9 +47,14 @@ return {
   {
     'ThePrimeagen/refactoring.nvim',
     dependencies = {
-      'nvim-lua/plenary.nvim',
-      'nvim-treesitter/nvim-treesitter',
+      'lewis6991/async.nvim',
     },
-    config = function() require('refactoring').setup {} end,
-  },
+    lazy = false,
+    init = function()
+      vim.keymap.set({ 'n', 'x' }, '<leader>rs', function()
+        -- this keymap doesn't select any textobject by default, so you may need to provide one each time you use it.
+        require('refactoring').select_refactor()
+      end, { desc = 'Select refactor' })
+    end,
+  }
 }
