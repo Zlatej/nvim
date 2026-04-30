d3c6aaa29b690bf73b2b0f0ca76e76f5e7778a5b
Matej Skalicky <matej.skalickyy@gmail.com>
2026-04-30 23:50:30 +0200

    merge idk

--- lua/custom/plugins/barvy.lua
+++ lua/custom/plugins/barvy.lua
@@ -6,4 +6,26 @@ return {
       vim.cmd.colorscheme 'vague'
     end,
   },
+  -- {
+  --   'binbandit/achroma.nvim',
+  --   lazy = false,
+  --   priority = 1000,
+  --   config = function() vim.cmd.colorscheme 'achroma' end,
+  -- },
+  --
+  -- { 'kdheepak/monochrome.nvim', config = function() vim.cmd 'colorscheme monochrome' end },
+  -- {
+  --   'zenbones-theme/zenbones.nvim',
+  --   -- Optionally install Lush. Allows for more configuration or extending the colorscheme
+  --   -- If you don't want to install lush, make sure to set g:zenbones_compat = 1
+  --   -- In Vim, compat mode is turned on as Lush only works in Neovim.
+  --   dependencies = 'rktjmp/lush.nvim',
+  --   lazy = false,
+  --   priority = 1000,
+  --   -- you can set set configuration options here
+  --   config = function()
+  --     -- vim.g.zenbones_darken_comments = 45
+  --     vim.cmd.colorscheme 'zenbones'
+  --   end,
+  -- },
 }
