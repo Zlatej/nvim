9a9a22c1584fd2ddb3054b6a88cb9e80cd2cc045
Matej Skalicky <matej.skalickyy@gmail.com>
2026-05-28 21:19:00 +0200

    <> autoparis fix

--- lua/kickstart/plugins/autopairs.lua
+++ lua/kickstart/plugins/autopairs.lua
@@ -2,4 +2,32 @@
 -- https://github.com/windwp/nvim-autopairs
 
 vim.pack.add { 'https://github.com/windwp/nvim-autopairs' }
-require('nvim-autopairs').setup {}
+
+local npairs = require 'nvim-autopairs'
+local Rule = require 'nvim-autopairs.rule'
+local cond = require 'nvim-autopairs.conds'
+
+npairs.setup {}
+
+-- Pair `<>` for templates/generics. Scoped to languages where `<` is commonly
+-- a generic delimiter, and only when preceded by a word char (e.g. `vector<`
+-- triggers, `a < b` does not).
+npairs.add_rule(Rule('<', '>', { 'cpp', 'c', 'rust', 'hpp', 'h', 'cc', 'cxx' }):with_pair(cond.before_regex '[%w_]'):with_move(function(opts)
+  return opts.char == '>'
+end))
+
+-- rust-analyzer (loaded eagerly by rustaceanvim) finishes attaching after
+-- autopairs and asynchronously deletes the buffer-local `>` skip-over mapping.
+-- Re-assert autopairs' keymaps shortly after any LSP attaches so the closing
+-- `>` survives.
+vim.api.nvim_create_autocmd('LspAttach', {
+  callback = function(args)
+    vim.defer_fn(function()
+      if vim.api.nvim_buf_is_valid(args.buf) then
+        vim.api.nvim_buf_call(args.buf, function()
+          npairs.force_attach()
+        end)
+      end
+    end, 300)
+  end,
+})
