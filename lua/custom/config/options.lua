d393f5cb4f008c6a8e1dffc42922c599d065543e
Matej Skalicky <matej.skalickyy@gmail.com>
2026-09-01 08:24:13 +0200

    fmt

--- lua/custom/config/options.lua
+++ lua/custom/config/options.lua
@@ -1,12 +1,12 @@
-vim.o.colorcolumn = "120"
+vim.o.colorcolumn = '120'
 
 -- termguicolors must be set, yapyapyap
 vim.o.termguicolors = true
 
 -- cycle lines
-vim.cmd("set whichwrap+=h,l")
-vim.cmd("set whichwrap+=<,>,[,]")
-vim.o.mouse = "a"
+vim.cmd 'set whichwrap+=h,l'
+vim.cmd 'set whichwrap+=<,>,[,]'
+vim.o.mouse = 'a'
 
 -- -- Tab
 vim.o.tabstop = 4
@@ -40,37 +40,35 @@ vim.o.smartindent = true
 -- [[ Basic Keymaps ]]
 
 -- Clear highlights on search when pressing <Esc> in normal mode
-vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
 -- Diagnostic keymaps - might be overriding with vim.diagnostic.config in init.lua
 -- vim.diagnostic.config({ virtual_text = true, float = { border = "rounded" } })
 
 -- Add borders to LSP hover (Shift+K) and signature help windows
-vim.keymap.set("n", "K", function()
-	vim.lsp.buf.hover({ border = "rounded" })
-end, { desc = "LSP Hover" })
+vim.keymap.set('n', 'K', function() vim.lsp.buf.hover { border = 'rounded' } end, { desc = 'LSP Hover' })
 
 -- Keybinds to make split navigation easier.
-vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
-vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
-vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
-vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
+vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
 -- Yank to system clipboard
-vim.api.nvim_set_keymap("n", "Y", '"+y', { noremap = true, silent = true })
-vim.api.nvim_set_keymap("v", "Y", '"+y', { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "YY", '"+yy', { noremap = true, silent = true })
-vim.api.nvim_set_keymap("v", "YY", '"+yy', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', 'Y', '"+y', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', 'Y', '"+y', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', 'YY', '"+yy', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', 'YY', '"+yy', { noremap = true, silent = true })
 
 -- Open main.* file
-vim.keymap.set("n", "<leader>m", function()
-	local files = vim.fn.glob("main.*", false, true)
-	if #files > 0 then
-		vim.cmd("e " .. files[1])
-	else
-		print("No main file found in current directory")
-	end
-end, { desc = "Open main file in current directory" })
+vim.keymap.set('n', '<leader>m', function()
+  local files = vim.fn.glob('main.*', false, true)
+  if #files > 0 then
+    vim.cmd('e ' .. files[1])
+  else
+    print 'No main file found in current directory'
+  end
+end, { desc = 'Open main file in current directory' })
