3b36d3da4657beb888d43ccfcbcd0216e3b1bd08
Matej Skalicky <matej.skalickyy@gmail.com>
2026-09-06 10:13:24 +0200

    fix: leftover import

--- init.lua
+++ init.lua
@@ -1110,7 +1110,6 @@ do
   require 'kickstart.plugins.lint'
   require 'kickstart.plugins.autopairs'
   require 'kickstart.plugins.neo-tree'
-  require 'kickstart.plugins.gitsigns' -- adds gitsigns recommended keymaps
 
   -- NOTE: You can add your own plugins, configuration, etc. in `lua/custom/plugins/*.lua`.
   --
